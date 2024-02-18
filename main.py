82a20b73ff2637bd90b3ccfc33e2f351d6fc9639
miguelm7 <miguelmore100@gmail.com>
2024-02-18 18:50:45 +0100

    use json file as test database

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from typing import Text, Optional
 from datetime import datetime
 from uuid import uuid4 as uuid
 
+from helpers import read_json_data, save_json_data
+
 app = FastAPI()
 
-posts = []
+# posts = []
 
 # Post model
 class Post(BaseModel):
@@ -27,10 +29,12 @@ def read_root():
 
 @app.get("/posts")
 def get_posts():
+    posts = read_json_data("data")
     return posts
 
 @app.get("/posts/{post_id}")
 def get_post(post_id: str):
+    posts = read_json_data("data")
     for post in posts:
         if post["id"] == post_id:
             return post
@@ -38,24 +42,30 @@ def get_post(post_id: str):
 
 @app.post("/posts")
 def create_post(post: Post):
+    posts = read_json_data("data")
     post.id = str(uuid())
     posts.append(post.model_dump())
+    save_json_data("data",posts)
     return posts[-1]
 
 @app.delete("/posts/{post_id}")
 def delete_post(post_id: str):
+    posts = read_json_data("data")
     for idx, post in enumerate(posts):
         if post["id"] == post_id:
             deleted_post = posts.pop(idx)
+            save_json_data("data",posts)
             return {"successfully deleted" : deleted_post} 
     raise HTTPException(status_code=404, detail="Post not Found")
 
 @app.put("/posts/{post_id}")
 def update_post(post_id: str, updated_post: Post):
+    posts = read_json_data("data")
     for idx, post in enumerate(posts):
         if post["id"] == post_id:
             posts[idx]["title"] = updated_post.title
             posts[idx]["content"] = updated_post.content
             posts[idx]["author"] = updated_post.author
+            save_json_data("data",posts)
             return {"successfully updated" : posts[idx]}
     raise HTTPException(status_code=404, detail="Post not Found")
